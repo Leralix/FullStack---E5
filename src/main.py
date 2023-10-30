6e530624bc91103232c966a17ef94d2474030574
Leralix <leralix98@gmail.com>
2023-10-30 23:47:51 +0100

    Version test fonctionelle

--- src/main.py
+++ src/main.py
@@ -1,35 +1,79 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Depends, Request, Form, status
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from sqlalchemy import text
-from database import SessionLocal, engine, get_all_values
-from models import create_table, add_value
+from sqlalchemy.orm import Session
+from starlette.responses import RedirectResponse
+
+import database
+import models
 
 import os
 
-app = FastAPI()
 
 templates = Jinja2Templates(directory="static")
+
+app = FastAPI()
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-@app.get("/")
-async def root():
-    value = os.environ.get("TEST")
-    return {"message": "Hello world",
-            "database": engine.name}
-
-    # return templates.TemplateResponse("test.html", {"request": request, "valueTest": value})
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.on_event("startup")
 async def startup():
-    create_table(engine)
+    models.Base.metadata.create_all(bind=database.engine)
 
-    # add_value(engine, "Roger")
+    database.add_value(database.engine, "Roger")
     # add_value(engine, "Pedro")
     # add_value(engine, "Jacques")
+    return
+
+
+@app.get("/")
+def home(request: Request, db: Session = Depends(get_db)):
+    # value = os.environ.get("TEST")
+    user_list = db.query(models.User).all()
+    return templates.TemplateResponse("base.html",
+                                      {"request": request, "user_list": user_list})
+
+    # return templates.TemplateResponse("test.html", {"request": request, "valueTest": value})
+
+
+@app.post("/add")
+def add_user(name: str = Form(...), db: Session = Depends(get_db)):
+    print(name)
+    new_user = models.User(name=name)
+    db.add(new_user)
+    db.commit()
+
+    url = app.url_path_for("home")
+    return RedirectResponse(url=url, status_code=status.HTTP_303_SEE_OTHER)
+
+@app.get("/update/{user_id}")
+def update_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    user.is_checked = not user.is_checked
+    db.commit()
+
+    url = app.url_path_for("home")
+    return RedirectResponse(url=url, status_code=status.HTTP_302_FOUND)
+
+@app.get("/delete/{user_id}")
+def delete(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    db.delete(user)
+    db.commit()
+
+    url = app.url_path_for("home")
+    return RedirectResponse(url=url, status_code=status.HTTP_302_FOUND)
+
 
 
 @app.get("/form", response_class=HTMLResponse)
@@ -42,13 +86,13 @@ async def add_value_route(value: str):
     if value == None:
         return {"message": "Value is empty"}
 
-    add_value(engine, value)
+    database.add_value(database.engine, value)
     return {"message": "Value added successfully"}
 
 
 @app.get("/get_values/")
 async def get_values_route():
-    all_values = get_all_values()
+    all_values = database.get_all_values()
     return {"values": all_values}
 
 
--- src/models.py
+++ src/models.py
@@ -1,23 +1,14 @@
-from sqlalchemy import create_engine, Column, Integer, String, Sequence
+from sqlalchemy import Column, Integer,Boolean, String, Sequence
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
 
-class UserTable(Base):
+class User(Base):
     __tablename__ = 'table_test_user'
-    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
-    name = Column(String(50))
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    is_checked = Column(Boolean, default=False)
 
 
-def create_table(engine):
-    Base.metadata.create_all(engine)
-
-
-def add_value(engine, value):
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    nouvelle_valeur = UserTable(name=value)
-    session.add(nouvelle_valeur)
-    session.commit()
