6ee218f6a175958a82e6d3baf43b978581a9ac68
Gabe-MGNT <107551867+Gabe-MGNT@users.noreply.github.com>
2023-11-25 10:46:26 +0100

    Add authent

--- app/frontend/main.py
+++ app/frontend/main.py
@@ -11,12 +11,24 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static", html=True), name="static")
 templates = Jinja2Templates(directory="templates")
 
+def get_userinfo(request: Request):
+    access_token = request.cookies.get("Authorization")
+
+    try:
+        if access_token:
+            userinfo = keycloak_openid.userinfo(access_token)
+            return userinfo
+        else:
+            return None
+    except Exception as e:
+        return None
 
 
 @app.get("/")
-async def welcome(request: Request):
+async def welcome(request: Request, userinfo=Depends(get_userinfo)):
+    print("USERINFO : ",userinfo)
     return templates.TemplateResponse("welcome.html",
-                                      {"request": request})
+                                      {"request": request, "userinfo":userinfo})
 
 
 @app.get("/register")
@@ -78,7 +90,6 @@ app.add_middleware(
 def display_userinfo(token:str=Depends(oauth2_scheme)):
     userinfo = keycloak_openid.userinfo(token)
     return {"userinfo": userinfo}
-
 # FIN AJOUT AUTHENTIFICATION
 
 
@@ -87,13 +98,18 @@ def display_userinfo(token:str=Depends(oauth2_scheme)):
 
 
 
+
+
+
+from fastapi import FastAPI, Request, Response, Depends, HTTPException, status
+
 @app.get("/login")
 def login_page(request: Request):
     return RedirectResponse("http://localhost:8080/realms/myrealm/protocol/openid-connect/auth?client_id=myclient&response_type=code&scope=openid&redirect_uri=http://localhost:5000/login/callback/")
     #return templates.TemplateResponse("new_login.html",{"request": request})
 
 @app.get("/login/callback/")
-def login_callback(request: Request):
+def login_callback(request: Request, response: Response):
     code = request.query_params.get("code")
     if code is None:
         return {"Info":"No Token"}
@@ -101,35 +117,16 @@ def login_callback(request: Request):
         #token = keycloak_openid.token(code=code, grant_type="authorization_code", redirect_uri="http://localhost:5000/login/callback/")
         token = keycloak_openid.token(username="myuser", password="test")
         request.session["Authorization"] = token['access_token']
+        response.set_cookie(key="Authorization", value=token['access_token'], httponly=True, max_age=3600)
         print("TOKEN :",token)
         return {"Info":token['access_token']}
 
 
-"""
-@app.middleware('http')
-async def some_middleware(request: Request, call_next):
-    # update request headers
-    headers = dict(request.scope['headers'])
-    headers = {}
-    try:
-        print("looking for token")
-        print("initial header :",headers)
-        #headers['Authorization'] = b'Bearer ' + request.session.get("token").encode('utf-8')
-        headers[b'Authorization'] = 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI4ZndXR1hQekNJU2VSSlFiMlY1Nlg1Qzk4aFdUeDh0emhrVzZQTmZBeFMwIn0.eyJleHAiOjE3MDA4MTg2NDQsImlhdCI6MTcwMDgxODM0NCwianRpIjoiZGZmZmI5YzMtNmQ5NC00YWZiLWFjYzktN2ZkOGZhMGJjZjJhIiwiaXNzIjoiaHR0cDovL2tleWNsb2FrOjgwODAvcmVhbG1zL215cmVhbG0iLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiY2JlZjJiZDUtMWFiZi00YzgwLWJkYTMtYzViMzU4YjgzNGU4IiwidHlwIjoiQmVhcmVyIiwiYXpwIjoibXljbGllbnQiLCJzZXNzaW9uX3N0YXRlIjoiMTVlZjUwNzUtYmM5Ny00Nzk4LWJiMmEtMmZhYjgyZmU5ODc2IiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwOi8vbG9jYWxob3N0OjgwODEvKiIsImh0dHBzOi8vd3d3LmtleWNsb2FrLm9yZy8iXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbImRlZmF1bHQtcm9sZXMtbXlyZWFsbSIsIm9mZmxpbmVfYWNjZXNzIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6Im9wZW5pZCBlbWFpbCBwcm9maWxlIiwic2lkIjoiMTVlZjUwNzUtYmM5Ny00Nzk4LWJiMmEtMmZhYjgyZmU5ODc2IiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJuYW1lIjoidXNlcm5hbWUgdXNlcmxhc3RuYW1lIiwicHJlZmVycmVkX3VzZXJuYW1lIjoibXl1c2VyIiwiZ2l2ZW5fbmFtZSI6InVzZXJuYW1lIiwiZmFtaWx5X25hbWUiOiJ1c2VybGFzdG5hbWUifQ.Knh0vkLs2PgrMWO69igrvqOpS4_xAPCy0t8es94lmGccGosVVAm44NfN6BdkgmVdYgzJ6I2HEiHr_9BMvX5C7wEQQdmnM1uE5S-FD9uJtt9kWiBTAXJ2SXesdWq57WkPV46QyCAK0PWwfRqNc2SqbIpY9ftjJnFyi8TF6w0i0Lrw2h3lQdCMtYIjHrJ4sJYP1vNOkQbPkXHABM8DsSEAYHj9i8Tm3j8BD6Y8WBw1DhUFRlqhPm8t6XVnn4iSZ7U12XWZ7ihBo6XAW0B0ffv2PlM9R9vSGYL1D7QkZAFgap1c8MMVeIsCU3QTfcHAK6RcHp1bkS_o8_bApN8TSBMQkQ'.encode('utf-8')
-        print("new headers :",headers)
-        print(headers['Authorization'])
-        print("token founded")
-        request.scope['headers'] = [(k, v) for k, v in headers.items()]   
-        print("TOken found")
-    except Exception as e:
-        print("No token found")   
-    print(request)
-    return await call_next(request)
-"""
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.types import Scope, Receive, Send
 from starlette.requests import Request
 from starlette.responses import Response
+from fastapi import FastAPI, Cookie, HTTPException, Request
 
 @app.middleware("http")
 async def create_auth_header(
@@ -143,15 +140,12 @@ async def create_auth_header(
     """
 
 
-    print('Cookies')
-    print(request.session)
+    access_token = request.cookies.get("Authorization")
     if ("Authorization" not in request.headers 
-        and "Authorization" in request.session
+        and access_token
         ):
 
-        print("if loop")
-        print("----------------------------------")
-        access_token = request.session["Authorization"]
+        #access_token = request.session["Authorization"]
         
         request.headers.__dict__["_list"].append(
             (
@@ -159,7 +153,6 @@ async def create_auth_header(
                  f"Bearer {access_token}".encode(),
             )
         )
-        print(request.headers)
     
     response = await call_next(request)
     return response
